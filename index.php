0d021d5b5ab9a917ee6356a55b72e47049790a29
TrojanB <qwertroya@gmail.com>
2023-03-01 12:28:05 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sklep papierniczy</title>
+    <link rel="stylesheet" href="styl.css">
+
+</head>
+<body>
+
+    <div id="baner">
+        <h1>W naszym sklepie internetowym kupisz najtaniej</h1>
+    </div>
+
+    <div id="lewy">
+        <h3>Promocja 15% obejmuje artykuły:</h3>
+
+        <ul>
+            <li>Gumka do mazania</li>
+            <li>Cienkopis</li>
+            <li>Pisaki 60szt.</li>
+            <li>Markery 4 szt.</li>
+        </ul>
+    </div>
+
+    <div id="srodkowy">
+        <h3>Cena wybranego artykułu w promocji</h3>
+        <form method="POST" action="">
+            <select>
+                <option value="gumka">Gumka do mazania</option>
+                <option value="cienkopis">Cienkopis</option>
+                <option value="pisaki">Pisaki 60szt.</option>
+                <option value="markery">Markery 4szt.</option>
+            </select>
+            <input type="submit" value="WYBIERZ">
+        </form>
+    </div>
+    <div id="prawy">
+        <h3>Kontakt</h3>
+        <p>telefon: 123123123 <br> e-mail: <a href="">bok@sklep.pl</a> </p>
+        <img src="./materialy1/promocja2.png" alt="promocja">
+
+    </div>
+
+    <footer><h4>Autro strony: Trojan Bogacki 3pir</h4></footer>
+</body>
+</html>
